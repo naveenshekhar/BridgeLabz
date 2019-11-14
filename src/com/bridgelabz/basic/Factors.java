ac6f8e3b2fffd57bf16134bf248bb6c3d343140f
naveenshekhar <56026439+naveenshekhar@users.noreply.github.com>
2019-11-14 18:29:58 +0530

    commit on 14:09:2019

--- src/com/bridgelabz/basic/Factors.java
+++ src/com/bridgelabz/basic/Factors.java
@@ -7,13 +7,13 @@
  * */
 package com.bridgelabz.basic;
 
-import java.util.Scanner;
+import com.bridgelabz.utility.Utility;
 
 public class Factors {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Enter your number to get prime factors :");
-		int n = sc.nextInt();
+		int n = Utility.sc.nextInt();
 
 		for (int i = 2; i < n; i++) {
 			while (n % i == 0) {
@@ -22,8 +22,6 @@ public class Factors {
 			}
 		}
 		System.out.println(n);
-		sc.close();
-
 	}
 
 }
