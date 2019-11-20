75f4dda8c6c0f6fcd1448cf03e420d08565033ae
naveen <naveen@naveen>
2019-11-20 09:25:37 +0530

    commited

--- src/com/bridgelabz/algorithm/AnagramString.java
+++ src/com/bridgelabz/algorithm/AnagramString.java
@@ -7,6 +7,8 @@ public class AnagramString {
 		System.out.println("Enter first String :");
 		String s1=Utility.sc.next();
 		System.out.println("Enter Second String :");
+		
+		
 		String s2=Utility.sc.next();
 		
 		Utility.isAnagram(s1, s2);
--- src/com/bridgelabz/algorithm/Palindrome.java
+++ src/com/bridgelabz/algorithm/Palindrome.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.algorithm;
 
+import com.bridgelabz.utility.Utility;
+
 public class Palindrome <T> 
 {
 	public static void main(String[] args) 
 	{
+		int  n=Utility.sc.nextInt();
 			
 		
 	}
