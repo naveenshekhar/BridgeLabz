9994d67a2900c1729d7a0769ffae71f7e2546ff6
naveenshekhar <naveen.shekhar624@gmail.com>
2019-11-21 18:27:16 +0530

    LinkedList methods created.

--- src/com/bridgelabz/dataStructure/UnOrderedList.java
+++ src/com/bridgelabz/dataStructure/UnOrderedList.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.dataStructure;
 
+//import com.bridgelabz.dataStructure.base.LinkedList;
+
 public class UnOrderedList 
 {
-	
+	public static void main(String[] args) 
+	{
+//		Object o=new Object();
+//		Integer i=new Integer(1111);
+//		LinkedList list=new LinkedList();
+//		list.add(10);
+//		list.add(20);
+//		list.add(o);
+//		list.show();
+//		
+	}
 
 }
--- src/com/bridgelabz/dataStructure/base/LinkedList.java
+++ src/com/bridgelabz/dataStructure/base/LinkedList.java
@@ -1,7 +1,124 @@
 package com.bridgelabz.dataStructure.base;
 
-public class LinkedList 
-{
+public class LinkedList<T> {
+	Node head;
+
+	public class Node {
+		Node next;
+		T data;
+
+		public Node(T data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public void add(T data) {
+		Node node = new Node(data);
+		Node temp = head;
+
+		if (head == null) {
+			head = node;
+		} else {
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = node;
+		}
+	}
+	/**
+	 * removing from end of linked list.
+	 */
+	public void pop()
+	{
+		Node temp=head;
+		if(head==null)
+		{
+			System.out.println("List is Empty..!!");
+		}
+		else
+		{
+			while(temp.next.next!=null)
+			{
+				temp=temp.next;
+			}
+			System.out.println("Removed element :"+temp.next.data);
+			temp.next=null;
+		}
+	}
 	
+	/**
+	 * Removing from list
+	 */
+	public void remove()
+	{
+		Node temp=head;
+		if(head==null)
+		{
+			System.out.println("List is Empty..!!");
+		}
+		else
+		{
+			while(temp.next.next!=null)
+			{
+				temp=temp.next;
+			}
+			System.out.println("Removed element :"+temp.next.data);
+			temp.next=null;
+		}
+	}
+	/**
+	 * searching the item
+	 */
+	public boolean search(T item)
+	{
+		Node temp=head;
+		if(head==null)
+		{
+			System.out.println("List is empty..!!");
+		}
+		else
+		{
+			while(temp.data!=item)
+			{
+				
+			}
+		}
+		return false;
+	}
+
+	public  void show() {
+		Node temp = head;
+		if (head == null) {
+			System.out.println("list is empty..!!");
+		} else {
+			while (temp.next != null) {
+				System.out.print(temp.data+" ");
+				temp = temp.next;
+			}
+			System.out.println(temp.data);
+		}
+	}
+
+//	public static void main(String[] args) {
+//		LinkedList list = new LinkedList();
+//		
+//		list.add("bhai");
+//		list.add("nana");
+//		list.add("kaka");
+//		list.add("lala");
+//		list.add(5.5);
+//		list.add(10);
+//		list.add(20);
+//		list.add(30);
+//		list.add(40);
+//		list.add(50);
+//		list.add(60);
+//		list.show();
+//		list.pop();
+//		list.remove();
+//		list.show();
+//
+//	}
 
 }
