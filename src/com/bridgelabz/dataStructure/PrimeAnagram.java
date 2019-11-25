726c7b41eb061c05e87e0b7d60e9100877ce9801
naveenshekhar <naveen.shekhar624@gmail.com>
2019-11-25 15:54:02 +0530

    primeAnagram

---
+++ src/com/bridgelabz/dataStructure/PrimeAnagram.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.dataStructure;
+
+import com.bridgelabz.utility.DSUtility;
+
+public class PrimeAnagram {
+	public static void main(String[] args) {
+		int arr[] = new int[1000];
+		// int arr2D[][] = new int[10][100];
+		int range = 2;
+		for (int i = 0; i < arr.length; i++) {
+			if (DSUtility.isPrime(range)) {
+
+				arr[i] = range;
+			}
+			range++;
+		}
+		for (int k = 0; k < arr.length / 10; k++) {
+			for (int j = k + 1; j < arr.length / 10; j++) {
+				if (DSUtility.isAnagram(arr[k], arr[j]) && arr[k] != 0 && arr[j] != 0) {
+					System.out.println(arr[k] + " " + arr[j]);
+				}
+			}
+		}
+	}
+}
--- src/com/bridgelabz/utility/DSUtility.java
+++ src/com/bridgelabz/utility/DSUtility.java
@@ -16,12 +16,6 @@ public class DSUtility {
 		return res;
 	}
 
-	public static void main(String[] args) {
-
-		System.out.println(isAnagram(117, 171));
-
-	}
-
 	public static int count(int n) {
 		String str1 = String.valueOf(n);
 		int l1 = str1.length();
@@ -39,22 +33,34 @@ public class DSUtility {
 		int j = 0;
 		int n1[] = new int[l1];
 		int n2[] = new int[l2];
-		while (n != 0 && m != 0) {
+		while (n != 0) {
 			int r = n % 10;
-			int e = m % 10;
 			n1[i] = r;
-			n2[j] = e;
 			n /= 10;
-			m /= 10;
 			i++;
+		}
+		j=0;
+		while (m != 0) {
+			int e = m % 10;
+			n2[j] = e;
+			m /= 10;
 			j++;
 		}
 		Arrays.sort(n1);
 		Arrays.sort(n2);
+		
 		if (Arrays.equals(n1, n2))
 			return true;
 		else
 			return false;
+		
+		
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println(isAnagram(0,1));
+		
+		
 	}
 
 }
