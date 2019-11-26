8511da26852e8652a47e08076ab8651ccd32a048
naveenshekhar <naveen.shekhar624@gmail.com>
2019-11-26 11:15:07 +0530

    Banking cash counter is updated

--- src/com/bridgelabz/dataStructure/BalancedParanthesis.java
+++ src/com/bridgelabz/dataStructure/BalancedParanthesis.java
@@ -1,13 +1,24 @@
+/** **************************************************
+ * @Author  : Naveen Shekhar
+ * @version : 1.0
+ * @purpose : Balancing parenthesis.
+ * @Date    : 25:11:2019
+ * @File    : BalancedParanthesis.java
+ * ***************************************************/
+
+
 package com.bridgelabz.dataStructure;
 
 import com.bridgelabz.dataStructure.base.Stack;
 
 public class BalancedParanthesis {
 	public static void main(String[] args) {
+		
+//Stack object created
 		Stack<Object> bp = new Stack<Object>();
 		String input = "(5+6)*(7+8)/(4+3)(5+6)*(7+8)/(4+3)";
 		char ch[] = input.toCharArray();
-
+// adding and removing '(' and ')'.
 		for (char in : ch) {
 			if (in == '(') {
 				bp.push('(');
@@ -15,7 +26,7 @@ public class BalancedParanthesis {
 				bp.pop();
 			}
 		}
-
+    //to check list is empty or not
 		if (bp.isEmpty() == true) {
 			System.out.println("true");
 		} else {
--- src/com/bridgelabz/dataStructure/BankingCashCounter.java
+++ src/com/bridgelabz/dataStructure/BankingCashCounter.java
@@ -1,3 +1,12 @@
+/** **************************************************
+ * @Author  : Naveen Shekhar
+ * @version : 1.0
+ * @purpose : To mimic the banking cash counter using Queue
+ *            present in Base package.
+ * @Date    : 25:11:2019
+ * @File    : BankingCashCounter.java
+ * ***************************************************/
+
 package com.bridgelabz.dataStructure;
 
 import com.bridgelabz.dataStructure.base.Queue;
@@ -8,7 +17,9 @@ public class BankingCashCounter
 	public static void main(String[] args) 
 	{
 		Queue<Integer> q=new Queue<Integer>();
-		int amount=1000000;
+//Initial amount in bank
+		int amount=1000000;		
+//Tacking input for people in queue
 		System.out.println("Enter how many people are in queue :");
 		int people=Utility.sc.nextInt();
 		for(int i=0;i<people;i++)
@@ -16,6 +27,7 @@ public class BankingCashCounter
 			q.enqueue(i);
 			System.out.println("Enter 1 if you want to deposite enter 2 if you want to withdrawl :");
 			int purpous=Utility.sc.nextInt();
+//withdrawl and deposit by each one in queue.
 			switch(purpous)
 			{
 			case 1:
