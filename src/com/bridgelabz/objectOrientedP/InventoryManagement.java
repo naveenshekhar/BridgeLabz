79ed361458c34365f2b93929f51e205d323d24eb
naveenshekhar <naveen.shekhar624@gmail.com>
2019-11-28 17:48:12 +0530

    Inventory Management is done.

--- src/com/bridgelabz/objectOrientedP/InventoryManagement.java
+++ src/com/bridgelabz/objectOrientedP/InventoryManagement.java
@@ -1,52 +1,67 @@
+/** **************************************************
+ * @Author  : Naveen Shekhar
+ * @version : 1.0
+ * @purpose : Created a JSON file having Inventory Details for Rice,
+ * 			  Pulses and Wheats with properties name, weight, price per kg.
+ * 			  And read json file using parser.
+ * @Date    : 13:11:2019 - 19:11:2019
+ * @File    : InventoryManagement.java
+ * ***************************************************/
 package com.bridgelabz.objectOrientedP;
+
 import java.io.FileReader;
-import java.util.Iterator;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class InventoryManagement 
-{
-	@SuppressWarnings("unchecked")
-	public static void main(String[] args) 
-	{
-		JSONParser parser =new JSONParser();
-		
-		try
-		{
-			int total;
-//Reading the location of json file
-			Object obj=parser.parse(new FileReader("/home/naveen/git/BridgeLabz/src/com/bridgelabz/objectOrientedP/InventoryManagement.json"));
-			
-//Creating JSONObject by downcasting Object to convert the Object(obj) into json object
-			JSONObject jsonObject=(JSONObject) obj; 
-//Extracting json elements present in file
-			String name=(String) jsonObject.get("Name");
-//Displaying the output
-			System.out.println("Name is :"+name);
-			
-			String weight=(String) jsonObject.get("Weight");
-			System.out.println("Weight is :"+weight);
-			
-			String 	ratePerKG=(String) jsonObject.get("Rate/KG");
-			System.out.println("Rate per KG is :"+ratePerKG);
-			total=(Integer.parseInt(weight))*(Integer.parseInt(ratePerKG));
-			System.out.println("Total price of wheat :"+total);
-			
-//Loop through Array and print
-			JSONArray groceryArray = (JSONArray) jsonObject.get("grocery");
-			Iterator<String> iterator = groceryArray.iterator();
-			
-			while(iterator.hasNext())
-			{
-				System.out.println("grocery : "+iterator.hasNext());
-			}
-			
-			
+public class InventoryManagement {
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public static void main(String[] args) {
+
+		JSONObject obj = new JSONObject();
+		obj.put("Name", "Rice");
+		obj.put("weight", new Integer(50));
+		obj.put("price", new Integer(70));
+
+		JSONObject obj1 = new JSONObject();
+		obj1.put("Name", "Wheat");
+		obj1.put("weight", new Integer(50));
+		obj1.put("price", new Integer(40));
+
+		JSONObject obj2 = new JSONObject();
+		obj2.put("Name", "pulses");
+		obj2.put("weight", new Integer(50));
+		obj2.put("price", new Integer(120));
+
+		JSONArray inventory = new JSONArray();
+		inventory.add(obj);
+		inventory.add(obj1);
+		inventory.add(obj2);
+		// try-with-resources statement based on post comment below :)
+		try (FileWriter file = new FileWriter("/home/naveen/git/BridgeLabz/src/com/bridgelabz/objectOrientedP/test2.json")) {
+			file.write(inventory.toJSONString());
+			// System.out.println(inventory);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		catch(Exception e)
-		{
+		try {
+			JSONParser parser = new JSONParser();
+			Object parse = parser
+					.parse(new FileReader("/home/naveen/git/BridgeLabz/src/com/bridgelabz/objectOrientedP/test2.json"));
+			JSONArray JSON = (JSONArray) parse;
+
+			for (Object j : JSON) {
+				JSONObject N = (JSONObject) j;
+				System.out.println("Name :" + N.get("Name"));
+				System.out.println("Price :" + N.get("price"));
+				System.out.println("Weight :" + N.get("weight"));
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/com/bridgelabz/utility/Utility.java
+++ src/com/bridgelabz/utility/Utility.java
@@ -2,7 +2,7 @@
  * @Author  : Naveen Shekhar
  * @version : 1.0
  * @purpose : Utility(BASIC, ALGORITHM, FUNCTIONAL, JUNIT AND LOGICAL) 
- * @Date    : 13:11:2019 - 19:11:2019
+ * @Date    : 13:11:2019 - 26:11:2019
  * @File    : Utility.java
  * ***************************************************/
 package com.bridgelabz.utility;
