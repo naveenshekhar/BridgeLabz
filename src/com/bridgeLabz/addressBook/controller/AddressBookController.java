22cf0194082e908d4f126d76f577e62b4cf51e44
naveenshekhar <naveen.shekhar624@gmail.com>
2019-12-06 18:17:01 +0530

    Update

--- src/com/bridgeLabz/addressBook/controller/AddressBookController.java
+++ src/com/bridgeLabz/addressBook/controller/AddressBookController.java
@@ -10,26 +10,25 @@ import com.bridgeLabz.addressBook.services.AddressBookService;
 import com.bridgeLabz.addressBook.services.serviceImplementation.AddressBookServiceImpl;
 import com.bridgeLabz.addressBook.utility.Utility;
 
-
 public class AddressBookController {
 	static Utility utility = new Utility();
 	static AddressBookService bookService = new AddressBookServiceImpl();
 	static AddressBookRepository bookRepository = new AddressBookRepository();
 	static String name;
+
 	public static void main(String[] args) {
-		
+
 		AddressBookController.addressBookMenu();
-		//AddressBookController.personMenu();
-		
+		// AddressBookController.personMenu();
+
 	}
 
-	
 	public static void addressBookMenu() {
 		System.out.println("1. Add Address Book");
 		System.out.println("2. Delete Address Book");
 		System.out.println("3. Select Address Book");
 		String select = Utility.inputString();
-		if(Utility.stringChecker(select)) {
+		if (Utility.stringChecker(select)) {
 			switch (select) {
 			case "1":
 				System.out.println("Enter Address Book Name");
@@ -45,14 +44,14 @@ public class AddressBookController {
 				break;
 			case "3":
 				File file = bookService.listOfFiles();
-				//System.out.println("Controller :"+file);
 				personMenu(file);
-			break;
+				break;
 			default:
 				break;
 			}
 		}
 	}
+
 	public static void personMenu(File file) {
 		System.out.println("\nSelect following option to perform some action..\n");
 		System.out.println("1. Add a person");
@@ -61,10 +60,10 @@ public class AddressBookController {
 		System.out.println("4. Search a person");
 		System.out.println("5. Quit");
 		String select = Utility.inputString();
-		if(Utility.stringChecker(select)) {
+		if (Utility.stringChecker(select)) {
 			switch (select) {
 			case "1":
-				//AddressBookController.addUser();
+				// AddressBookController.addUser();
 				JSONObject temp = AddressBookServiceImpl.addUser(file);
 				AddressBookRepository.writeDataNew(temp, file);
 				personMenu(file);
@@ -78,13 +77,13 @@ public class AddressBookController {
 			case "3":
 				System.out.println("Enter first name :");
 				name = Utility.inputString();
-				bookService.delete(file,name);
+				bookService.delete(file, name);
 				personMenu(file);
 				break;
 			case "4":
 				System.out.println("Enter first name :");
 				name = Utility.inputString();
-				bookService.search(file,name);
+				bookService.search(file, name);
 				personMenu(file);
 				break;
 			case "5":
@@ -97,29 +96,27 @@ public class AddressBookController {
 	}
 
 	public static void addUser() {
-		String firstName,lastName,address,city,state;
+		String firstName, lastName, address, city, state;
 		String zip;
 		String mobile;
-		
+
 		@SuppressWarnings("static-access")
 		JSONObject jsonObject = bookRepository.readData();
 		jsonObject = (JSONObject) jsonObject.get("Address Book");
-		
-		
+
 		Person person = new Person();
 		System.out.println("Enter first Name :");
 		firstName = Utility.getString(false);
-		
-		
+
 		System.out.println("Enter Last Name :");
 		lastName = Utility.getString(false);
-		
+
 		System.out.println("Enter Address :");
 		address = Utility.getString(false);
-		
+
 		System.out.println("Enter city Name :");
 		city = Utility.getString(false);
-		
+
 		System.out.println("Enter State Name :");
 		state = Utility.getString(false);
 
@@ -128,21 +125,21 @@ public class AddressBookController {
 		if (Utility.stringChecker(zip)) {
 			person.setZip(Integer.parseInt(zip));
 		}
-		
+
 		System.out.println("Enter 10 digit Phone Number :");
 		mobile = Utility.inputString();
 		if (Utility.stringChecker(zip)) {
 			person.setPhonenumber(Long.parseLong(mobile));
 		}
-		
+
 		person.setFirstName(firstName);
 		person.setLastName(lastName);
 		person.setCity(city);
 		person.setAddress(address);
 		person.setState(state);
-		
+
 		bookService.store(person);
 		personMenu(null);
-		
+
 	}
 }
